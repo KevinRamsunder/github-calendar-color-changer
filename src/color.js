81c540ff279c03c5b9a56e3e8ba23bc4484723be
Kevin Ramsunder <kevinramsunder5@gmail.com>
2017-02-24 20:11:28 -0500

    Add progress bar color swapping

--- src/color.js
+++ src/color.js
@@ -24,6 +24,35 @@ function rgbToHex( rgb ) {
   ).join( '' );
 };
 
+// get the background-color property and update it
+function swapColorStyleProp( element ) {
+  const currentColor = rgbToHex( element.style[ 'background-color' ] );
+  element.setAttribute( 'style', 'background-color:' + colorSwap( currentColor ) );
+};
+
+// fancy sleep function
+function sleep( ms ) {
+  return new Promise( ( resolve ) => setTimeout( resolve, ms ) );
+};
+
+// change the progress bar colors
+async function walkAndSwapProgressBarColors() {
+  // do an initial wait so github can fetch the data and update the DOM
+  await sleep( 250 );
+
+  // do additional busy-waiting if the loading class is present on the DOM
+  const loadingBox = document.getElementById( 'js-contribution-activity' );
+  while ( loadingBox.className.includes( 'loading' ) ) {
+    await sleep( 250 );
+  }
+
+  // update the progress bar colors once they are on the DOM
+  const progressBars = Array.from( document.getElementsByClassName( 'progress-bar' ) );
+  progressBars.forEach( ( progressBar ) => {
+    swapColorStyleProp( progressBar );
+  } );
+};
+
 // change the github map colors
 function walkAndSwapMap() {
   // get the day squares on the github contribution map as an array
@@ -32,6 +61,8 @@ function walkAndSwapMap() {
   days.forEach( function( day ) {
     const newColor = colorSwap( day.getAttribute( 'fill' ) );
     day.setAttribute( 'fill', newColor );
+    // attach click event for squares so we can update the progress bars once they are loaded
+    day.addEventListener( 'click', walkAndSwapProgressBarColors );
   } );
 };
 
@@ -41,10 +72,10 @@ function walkAndSwapLegend() {
   const legendSquares = Array.from( document.getElementsByClassName( 'legend' )[ 0 ].children );
 
   legendSquares.forEach( ( square ) => {
-    const currentColor = rgbToHex( square.style[ 'background-color' ] );
-    square.setAttribute( 'style', 'background-color:' + colorSwap( currentColor ) );
+    swapColorStyleProp( square );
   } );
 };
 
 walkAndSwapMap();
 walkAndSwapLegend();
+walkAndSwapProgressBarColors();
